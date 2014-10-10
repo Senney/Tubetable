8a5d46e0ffde90a6d1b748881ccfe72a6b4e23d2
Sean Heintz <sean.heintz@gmail.com>
2014-10-09 21:07:05 -0600

    Refactoring and feature addition
    * split tubetable.js into two files for seperate functionalit.y
    * added some functionality to get the current video from the API.

--- static/js/tubetable.js
+++ static/js/tubetable.js
@@ -2,75 +2,7 @@
 var tag = $('<script></script>').attr({src: "https://www.youtube.com/iframe_api"});
 $("script:first-of-type").prepend(tag);
 
-var cache = {}
-var inWaiting = false;
-var current = undefined;
 
-function getNext(callback) {
-    $.ajax({
-        url: '/head',
-        dataType: 'json'
-    }).success(function(data) {
-        inWaiting = false;
-        current = data;
-        callback(data);
-    }).fail(function(data) {
-        if (!inWaiting) {
-            setTimeout(function() { getNext(callback) }, 1500);
-        }
-        inWaiting = true;
-    });
-}
-
-function getVideoList() {
-    $.ajax({
-        url: '/list',
-        dataType: 'json'
-    }).success(function(data) {
-        if (!current) return;
-
-        if (!$.isArray(data)) {
-            setVideoList([current, data]);
-        } else {
-            data.unshift(current);
-            setVideoList(data);
-        }
-    });
-}
-
-function getVideoName(id, callback) {
-    if (cache[id] != undefined) {
-        callback(cache[id]);
-        return;
-    }
-
-    name = "No video name found.";
-    cache[id] = "Pending";
-    $.ajax({
-        url: "https://gdata.youtube.com/feeds/api/videos/" + id + "?v=2&alt=json",
-        dataType: 'json'
-    }).success(function(data) {
-        name = data['entry']['title']['$t'];
-        callback(name);
-        cache[id] = name;
-    }).fail(function() {
-        callback(name);
-        cache[id] = name;
-    });
-}
-
-function setVideoList(data) {
-    console.log(data);
-    $("#queue").empty();
-    for (var v in data) {
-        tag = $("<li></li>")
-            .addClass("list-group-item");
-        $("#queue").append(tag);
-        getVideoName(data[v], function(name) {
-            tag.text(name);
-        });
-    }
-}
 
 /**
     This is the Youtube API boilerplate.
@@ -91,11 +23,7 @@ function onYouTubeIframeAPIReady() {
         });
     });
 
-    setInterval(getVideoList, 5000);
-}
-
-function playNext() {
-    getNext(function(next) { player.loadVideoById({videoId: next }); })
+    setInterval(getVideoList, 1500);
 }
 
 function onPlayerReady(event) {
@@ -105,6 +33,7 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.ENDED) {
+        current = undefined;
         playNext();
     }
 }
