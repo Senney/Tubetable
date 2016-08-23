70210129b37c18b8f38a3fed3a2e57f3ac3d766f
Sean Heintz <sean.heintz@gmail.com>
2016-08-22 22:21:12 -0600

    added example quickstart

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Tubetable - Angular Style!</h1>'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+// Angular imports
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Application imports
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
