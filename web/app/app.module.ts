f32276441c4a368f148a46ba67ed1d1ab38febe0
Sean Heintz <sean.heintz@gmail.com>
2016-08-24 23:11:19 -0600

    added api call to gather video data

--- web/app/app.module.ts
+++ web/app/app.module.ts
@@ -2,15 +2,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 // Application imports
 import { AppComponent } from './app.component';
 import { TubetableComponent } from './tubetable.component';
 import { VideoViewComponent } from './video-view.component';
+import { Configuration } from './app.constants';
 
 @NgModule({
-    imports: [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, TubetableComponent, VideoViewComponent ],
-    bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        TubetableComponent,
+        VideoViewComponent
+    ],
+    providers: [
+        Configuration
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
--- web/app/video.service.ts
+++ web/app/video.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
+import { Configuration } from './app.constants';
 import { QueuedVideo } from './queued-video';
-import { VIDEO_QUEUE } from './mock-videos';
 
 @Injectable()
 export class VideoService {
+
+    dataUrl: string;
+
+    constructor(private http: Http,
+                private configuration: Configuration) {
+        this.dataUrl = configuration.ApiServer + 'videos'
+    }
+
     getVideos(): Promise<QueuedVideo[]> {
-        return Promise.resolve(VIDEO_QUEUE);
+        return this.http.get(this.dataUrl)
+                   .toPromise()
+                   .then(response => response.json().data as QueuedVideo[])
+                   .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred while retrieving the data.', error);
+        return Promise.reject(error.message || error);
     }
 }
