6b971efb9a917b0de546d7f1c0466eb72661a4e3
Sean Heintz <sean.heintz@gmail.com>
2016-08-24 23:11:14 -0600

    Enabled xhr for api

--- api/tubetable_api.py
+++ api/tubetable_api.py
@@ -21,5 +21,15 @@ def get_videos():
     return jsonify({'data': videos})
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,HEAD')
+    return response
+
+
 if __name__ == '__main__':
     app.run(threaded=True)
